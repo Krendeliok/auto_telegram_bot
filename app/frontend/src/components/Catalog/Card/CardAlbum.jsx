294bd5d5372c033feb38b0555aa1933cf8835346
Kyrylo <kirillgapich@gmail.com>
2024-06-30 19:57:54 +0300

    small changes to catalog view

--- app/frontend/src/components/Catalog/Card/CardAlbum.jsx
+++ app/frontend/src/components/Catalog/Card/CardAlbum.jsx
@@ -1,8 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 function CardAlbum({ images }) {
 
     const mainImageRef = useRef()
+    const [currentImage, setCurrentImage] = useState(0);
 
     const setAsMain = (e) => {
         let mainImageSrc = mainImageRef.current.src; 
@@ -10,8 +11,18 @@ function CardAlbum({ images }) {
         e.target.src = mainImageSrc;
     }
 
+    const onLeftArrowClick = (e) => {
+        mainImageRef.current.src = images[(currentImage - 1 + images.length) % images.length].source;
+        setCurrentImage((prev) => (prev - 1 + images.length) % images.length)
+    }
 
-    return (  
+    const onRightArrowClick = (e) => {
+        mainImageRef.current.src = images[(currentImage + 1) % images.length].source;
+        setCurrentImage((prev) => (currentImage + 1) % images.length)
+    }
+
+
+    return (
         <div className="card__album">
             {
                 <div className="album__images">
@@ -26,19 +37,17 @@ function CardAlbum({ images }) {
                         )
                         : ""
                     }
+                    <div className="allcards__fogging"></div>
                 </div>
             }
+            <div className="carousel">
+                <div className="carousel__arrow" onClick={onLeftArrowClick}></div>
+                <div className="carousel__arrow right" onClick={onRightArrowClick}></div>
+                <div className="breadcrumbs">
+                    {images.map((image, index) => <div key={image.id} className={index === currentImage ? "active" : ""}></div>)}
+                </div>
+            </div>
             <div className="fogging"></div>
-            {/*{images.length > 1*/}
-            {/*    ? <div className="album__all-images">*/}
-            {/*        {images.slice(1).map(image => */}
-            {/*            <div className="album__image" key={image.id}>*/}
-            {/*                <img src={image.source} alt="" onClick={setAsMain}/>*/}
-            {/*            </div>*/}
-            {/*        )}*/}
-            {/*    </div>*/}
-            {/*    : ""*/}
-            {/*}*/}
         </div>
     );
 }
